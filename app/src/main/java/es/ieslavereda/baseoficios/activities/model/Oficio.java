49211b9b055ab2a51e1621daeb17813e663d697a
JuanTelloMartinez <juatelmar@alu.edu.gva.es>
2025-05-31 12:37:16 +0200

    31052025 - Model Oficio.java & Usuario.java

--- app/src/main/java/es/ieslavereda/baseoficios/activities/model/Oficio.java
+++ app/src/main/java/es/ieslavereda/baseoficios/activities/model/Oficio.java
@@ -1,4 +1,25 @@
 package es.ieslavereda.baseoficios.activities.model;
 
 public class Oficio {
+    private int idOficio;
+    private String descripcion;
+    private String image;
+
+    public Oficio(int idOficio, String descripcion, String image) {
+        this.idOficio = idOficio;
+        this.descripcion = descripcion;
+        this.image = image;
+    }
+
+    public int getIdOficio() {
+        return idOficio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getImage() {
+        return image;
+    }
 }
